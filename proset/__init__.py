4418001c107ffb2559ec8edb641ca5ab12ce06f7
Nikolaus Ruf <nikolaus.ruf@t-online.de>
2024-11-24 11:47:35 +0100

    Added pylint ignore for redundant import alias.

--- proset/__init__.py
+++ proset/__init__.py
@@ -4,6 +4,7 @@ Copyright by Nikolaus Ruf
 Released under the MIT license - see LICENSE file for details
 """
 
+# pylint: disable=useless-import-alias
 from proset.models.classifier_model import ClassifierModel as ClassifierModel
 
 
